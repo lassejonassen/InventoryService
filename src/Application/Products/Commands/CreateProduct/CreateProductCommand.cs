bf0a772c2aa52c26945aba7f052cad61aab1af00
Lasse Jonassen <lasjn@arlafoods.com>
2024-08-07 17:19:33 +0200

    Added Application.UnitTests

---
+++ src/Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,6 @@
+﻿
+using InventoryService.Application.Abstractions.Messaging;
+
+namespace InventoryService.Application.Products.Commands.CreateProduct;
+
+public sealed record CreateProductCommand(string Name, string? Description, string SKU, Guid ProductTypeId, Guid SupplierId) : ICommand<Guid>;
